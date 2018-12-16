4be73ae8215010de693621d1decd14a78e99717d
Michael Gale <m.gale@warwick.ac.uk>
2018-12-16 17:12:32 +0000

    Add 13 new tests and improve existing tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,8 @@
 -- Coursework 2: Scratch clone                                                --
 --------------------------------------------------------------------------------
 
+{-# LANGUAGE ScopedTypeVariables #-}
+
 import Control.Monad
 
 import Data.List
@@ -52,39 +54,53 @@ repeatTest n =
         [ AssignStmt "x" (BinOpE Add (VarE "x") (ValE 1)) ]
     ]
 
+-- | Constructs a test for an arbitary expression in the repeat statement.
+repeatArbitraryTest :: Expr -> Program
+repeatArbitraryTest expr =
+    [ RepeatStmt expr
+        [ AssignStmt "x" (BinOpE Add (VarE "x") (ValE 1)) ]
+    ]
+
 -- | Constructs a test for if statements.
-ifTest :: Int -> Program
-ifTest n =
-    [ IfStmt (BinOpE Equal (ValE 0) (ValE 0))
+rawIfTest :: Expr -> Int -> Program
+rawIfTest expr n =
+    [ IfStmt expr
         [ AssignStmt "x" (ValE n) ]
         []
         []
     ]
 
+-- | Constructs a test for if statements.
+ifTest :: Expr -> Int -> Program
+ifTest expr n = rawIfTest (BinOpE Equal expr expr) n
+
 -- | Constructs a test for else.
-elseTest :: Int -> Program
-elseTest n =
-    [ IfStmt (BinOpE Neq (ValE 0) (ValE 0))
+elseTest :: Expr -> Int -> Program
+elseTest expr n =
+    [ IfStmt (BinOpE Neq expr expr)
         [ AssignStmt "x" (ValE (n+1)) ]
         []
         [ AssignStmt "x" (ValE n) ]
     ]
 
 -- | Constructs a test for if else.
-ifElseTest :: Int -> Program
-ifElseTest n =
+rawIfElseTest :: Expr -> Int -> Program
+rawIfElseTest expr n =
     [ IfStmt (BinOpE Neq (ValE 0) (ValE 0))
         [ AssignStmt "x" (ValE (n+1)) ]
-        [ (BinOpE Equal (ValE 1) (ValE 1), [ AssignStmt "x" (ValE n) ]) ]
+        [ (expr, [ AssignStmt "x" (ValE n) ]) ]
         [ AssignStmt "x" (ValE (n-1)) ]
     ]
 
+-- | Constructs a test for if else.
+ifElseTest :: Expr -> Int -> Program
+ifElseTest expr n = rawIfElseTest (BinOpE Equal expr expr) n
+
+-- | 'fibs' is a stream of fibonacci numbers.
 fibs :: [Int]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
--- x = 0
--- y = 0
--- z = 1
+-- | 'fib' @n@ constructs a program which calculates the @n@th fibonacci number.
 fib :: Int -> Program
 fib n =
     [ RepeatStmt (ValE n)
@@ -95,6 +111,9 @@ fib n =
         ]
     ]
 
+-- tests to add
+-- * exception handling in nested contexts
+
 -- | The main entry point to the test suite.
 main :: IO ()
 main = do
@@ -103,31 +122,101 @@ main = do
             it "handles the empty program" $
                 interpret [] []
                 `shouldSatisfy` isSuccessful
-            it "stores values in memory" $
-                interpret [AssignStmt "x" (ValE 42)] []
-                `shouldSatisfy` hasMemory [("x",42)]
-            it "loads values from memory" $
-                interpret [AssignStmt "y" (VarE "x")] [("x",42)]
-                `shouldSatisfy` hasMemory [("x",42),("y",42)]
-            it "handles division by zero" $
-                interpret [AssignStmt "x" (BinOpE Div (ValE 42) (ValE 0))] []
-                `shouldBe` Left DivByZeroError
-            it "handles negative exponents" $
-                interpret [AssignStmt "x" (BinOpE Pow (ValE 42) (ValE (-1)))] []
-                `shouldBe` Left NegativeExponentError
-            it "handles uninitialised memory" $
+            prop "stores values in memory" $ \(x :: Int) ->
+                hasMemory [("x",x)] $ interpret [AssignStmt "x" (ValE x)] []
+            prop "loads values from memory" $ \(x :: Int) ->
+                hasMemory [("x",x),("y",x)] $
+                    interpret [AssignStmt "y" (VarE "x")] [("x",x)]
+            prop "memory only contains one entry per variable after multiple assignments" $
+                \(x :: Int) (y :: Int) (z :: Int) ->
+                    let r = interpret [ AssignStmt "x" (ValE x)
+                                      , AssignStmt "x" (ValE y)
+                                      , AssignStmt "x" (ValE z)
+                                      ] [("x",0)]
+                    in case r of
+                        Left _  -> property False
+                        Right m -> length (map fst m) ===
+                                   length (nub $ map fst m)
+            prop "handles division by zero in assignments" $ \(x :: Int) ->
+                interpret [AssignStmt "x" (BinOpE Div (ValE x) (ValE 0))] []
+                === Left DivByZeroError
+            prop "handles negative exponents in assignments" $ \(x :: Int) ->
+                interpret [AssignStmt "x" (BinOpE Pow (ValE x) (ValE (-1)))] []
+                === Left NegativeExponentError
+            it "handles uninitialised memory in assignments" $
                 interpret [AssignStmt "y" (VarE "x")] []
                 `shouldBe` Left (UninitialisedMemory "x")
-            prop "interprets expressions" $ \expr ->
+            prop "interprets arbitrary expressions in assignments" $ \expr ->
                 isSuccessful $ interpret [AssignStmt "x" expr] []
             prop "implements repeat" $ \(Positive n) ->
                 hasMemory [("x",n)] $ interpret (repeatTest n) [("x", 0)]
+            prop "repeat condition can contain arbitrary expressions" $ \expr ->
+                isSuccessful $ interpret (repeatArbitraryTest expr) [("x", 0)]
+            prop "repeat correctly handles exceptions in condition" $ \(x :: Int) ->
+                     interpret [RepeatStmt (BinOpE Div (ValE x) (ValE 0)) []] []
+                     === Left DivByZeroError
+                .&&. interpret [RepeatStmt (BinOpE Pow (ValE x) (ValE (-1))) []] []
+                     === Left NegativeExponentError
+                .&&. interpret [RepeatStmt (VarE "x") []] []
+                     === Left (UninitialisedMemory "x")
+            prop "repeat correctly handles exceptions in body" $ \(x :: Int) ->
+                     interpret [RepeatStmt (ValE 10) [AssignStmt "y" (BinOpE Div (ValE x) (ValE 0))]] []
+                     === Left DivByZeroError
+                .&&. interpret [RepeatStmt (ValE 10) [AssignStmt "y" (BinOpE Pow (ValE x) (ValE (-1)))]] []
+                     === Left NegativeExponentError
+                .&&. interpret [RepeatStmt (ValE 10) [AssignStmt "y" (VarE "x")]] []
+                     === Left (UninitialisedMemory "x")
             prop "implements if" $ \(Positive n) ->
-                hasMemory [("x",n)] $ interpret (ifTest n) [("x", 0)]
+                hasMemory [("x",n)] $ interpret (ifTest (ValE 0) n) [("x", 0)]
+            prop "if condition can contain arbitrary expressions" $ \(Positive n) expr ->
+                hasMemory [("x",n)] $ interpret (ifTest expr n) [("x", 0)]
+            prop "if condition treats non-zero numbers as true" $
+                forAll (arbitrary `suchThat` (/= 0)) $ \(n :: Int) ->
+                hasMemory [("x",n+1)] $ interpret (rawIfTest (ValE n) (n+1)) [("x", 0)]
+            prop "if correctly handles exceptions in condition" $ \(x :: Int) ->
+                     interpret [IfStmt (BinOpE Div (ValE x) (ValE 0)) [] [] []] []
+                     === Left DivByZeroError
+                .&&. interpret [IfStmt (BinOpE Pow (ValE x) (ValE (-1))) [] [] []] []
+                     === Left NegativeExponentError
+                .&&. interpret [IfStmt (VarE "x") [] [] []] []
+                     === Left (UninitialisedMemory "x")
+            prop "if correctly handles exceptions in body" $ \(x :: Int) ->
+                     interpret [IfStmt (ValE 1) [AssignStmt "y" (BinOpE Div (ValE x) (ValE 0))] [] []] []
+                     === Left DivByZeroError
+                .&&. interpret [IfStmt (ValE 1) [AssignStmt "y" (BinOpE Pow (ValE x) (ValE (-1)))] [] []] []
+                     === Left NegativeExponentError
+                .&&. interpret [IfStmt (ValE 1) [AssignStmt "y" (VarE "x")] [] []] []
+                     === Left (UninitialisedMemory "x")
             prop "implements else" $ \(Positive n) ->
-                hasMemory [("x",n)] $ interpret (elseTest n) [("x", 0)]
-            prop "implements if else" $ \(Positive n) ->
-                hasMemory [("x",n)] $ interpret (ifElseTest n) [("x", 0)]
+                hasMemory [("x",n)] $ interpret (elseTest (ValE 0) n) [("x", 0)]
+            prop "else correctly handles exceptions in body" $ \(x :: Int) ->
+                     interpret [IfStmt (ValE 0) [] [] [AssignStmt "y" (BinOpE Div (ValE x) (ValE 0))]] []
+                     === Left DivByZeroError
+                .&&. interpret [IfStmt (ValE 0) [] [] [AssignStmt "y" (BinOpE Pow (ValE x) (ValE (-1)))]] []
+                     === Left NegativeExponentError
+                .&&. interpret [IfStmt (ValE 0) [] [] [AssignStmt "y" (VarE "x")]] []
+                     === Left (UninitialisedMemory "x")
+            prop "implements else if" $ \(Positive n) ->
+                hasMemory [("x",n)] $ interpret (ifElseTest (ValE 1) n) [("x", 0)]
+            prop "else if condition can contain arbitary expressions" $ \(Positive n) expr ->
+                hasMemory [("x",n)] $ interpret (ifElseTest expr n) [("x", 0)]
+            prop "else if condition treats non-zero numbers as true" $
+                forAll (arbitrary `suchThat` (/= 0)) $ \(n :: Int) ->
+                hasMemory [("x",n+1)] $ interpret (rawIfElseTest (ValE n) (n+1)) [("x", 0)]
+            prop "else if correctly handles exceptions in condition" $ \(x :: Int) ->
+                     interpret [IfStmt (ValE 0) [] [(BinOpE Div (ValE x) (ValE 0), [])] []] []
+                     === Left DivByZeroError
+                .&&. interpret [IfStmt (ValE 0) [] [(BinOpE Pow (ValE x) (ValE (-1)), [])] []] []
+                     === Left NegativeExponentError
+                .&&. interpret [IfStmt (ValE 0) [] [(VarE "x", [])] []] []
+                     === Left (UninitialisedMemory "x")
+            prop "else if correctly handles exceptions in body" $ \(x :: Int) ->
+                     interpret [IfStmt (ValE 0) [] [(ValE 1, [AssignStmt "y" (BinOpE Div (ValE x) (ValE 0))])] []] []
+                     === Left DivByZeroError
+                .&&. interpret [IfStmt (ValE 0) [] [(ValE 1, [AssignStmt "y" (BinOpE Pow (ValE x) (ValE (-1)))])] []] []
+                     === Left NegativeExponentError
+                .&&. interpret [IfStmt (ValE 0) [] [(ValE 1, [AssignStmt "y" (VarE "x")])] []] []
+                     === Left (UninitialisedMemory "x")
         describe "Example programs" $ do
             prop "computes fibonacci numbers" $ \(Positive n) ->
                 hasMemory [("x", fibs !! (n-1))] $
